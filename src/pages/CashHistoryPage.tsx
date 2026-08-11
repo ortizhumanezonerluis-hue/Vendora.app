8c2852ac372ba992d7620ea40f911f07a08a655e
ortizhumanezeronerluis-hue <ortizhumanezonerluis@gmail.com>
2026-08-11 10:41:50 -0500

    fix: import useMemo from React in CashHistoryPage

--- src/pages/CashHistoryPage.tsx
+++ src/pages/CashHistoryPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import MainLayout from '../components/layout/MainLayout'
 import { cashService } from '../services/cashService'
 import { ArqueoCaja } from '../types'
